ecf7e0b3fe558f14103c0cf053efc28f596c8de3
Stepanryazan <offish.sk@gmail.com>
2024-05-05 16:45:04 +0300

    dz_cwb

--- catalog/views.py
+++ catalog/views.py
@@ -1,22 +1,44 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 
 from catalog.models import Product
 
 
-def home(request):
-    context = {'product_list': Product.objects.all()}
-    return render(request, 'catalog/home.html', context)
+class ProductListView(ListView):
+    model = Product
+    template_name = "catalog/product_list.html"
 
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'Message from {name} (Tel: {phone}): {message}')
-    return render(request, 'catalog/contacts.html')
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "catalog/product_detail.html"
 
 
-def product(request, pk):
-    context = {'product': get_object_or_404(Product, pk=pk)}
-    return render(request, 'catalog/product.html', context)
+class ProductCreateView(CreateView):
+    model = Product
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:home")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:home")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy("catalog:home")
+
+
+class ContactsView(TemplateView):
+    template_name = "catalog/contacts.html"
+
+    def post(self, request, *args, **kwargs):
+        if request.method == "POST":
+            name = request.POST.get("name")
+            phone = request.POST.get("phone")
+            message = request.POST.get("message")
+            print(f"Message from {name} (Tel: {phone}): {message}")
+        return render(request, self.template_name)
--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -21,6 +22,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(('catalog.urls', 'catalog'))),
+    path("admin/", admin.site.urls),
+    path("", include(("catalog.urls", "catalog"))),
+    path("blog/", include(("blog.urls", "blog")))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
